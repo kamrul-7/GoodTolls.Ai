f0fa24c0f9806b448dc0232504d459fbe5b3d688
kamrul-7 <kamrul7.khan@gmail.com>
2023-10-05 00:04:22 +0600

    test 2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1 className='text-2xl'>This is home and we working on it</h1>
+            <h2>Test for github </h2>
             
         </div>
     );
