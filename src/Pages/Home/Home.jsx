f150fd1f2a082d3741620a008ed591c7c9e5fd47
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-05 00:53:29 +0600

    chdsfh78

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             <h1 className='text-2xl'>This is home and we working on it</h1>
             <h2>Nothing Important</h2>
-            <h2>Serious issue</h2>
+            <h2>Serious issue mostly completed</h2>
             <h5>fdhgfhgt</h5>
         </div>
     );
