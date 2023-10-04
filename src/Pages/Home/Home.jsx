920366c99a478016b104419adba85a4327aa8668
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-05 00:27:26 +0600

    nothing special

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1 className='text-2xl'>This is home and we working on it</h1>
-            
+            <h2>Nothing Important</h2>
         </div>
     );
 };
