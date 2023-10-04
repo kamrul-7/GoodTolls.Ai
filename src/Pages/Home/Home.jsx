e7885cc269cf973198a502ebbe9a846e7676317a
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-05 03:23:49 +0600

    Trying to merge

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
         <div>
             <h1 className='text-2xl'>This is home and we working on it</h1>
             <h1>Git merge checking</h1>
+            <h1>now on feature branch</h1>
         </div>
     );
 };
