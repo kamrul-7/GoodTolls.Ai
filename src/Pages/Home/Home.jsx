d1479ab68792859a71f160d433d974a968aa4cfc
sajibkhansk <sajibkhan117593@gmial.com>
2023-10-07 00:33:26 +0600

    hero and footer section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from '../Card/Card';
+import Hero from '../Hero/Hero';
+import Footer from '../../Component/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <h1 className='text-2xl'>This is home and we working on it</h1>
+            <Hero></Hero>
           <Card/>
-            
+            <Footer></Footer>
         </div>
     );
 };
