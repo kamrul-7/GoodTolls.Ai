a6ce3d6346f1089536c2708247c4fbfca1713d25
kamrul-7 <kamrul7.khan@gmail.com>
2023-10-07 06:36:50 +0600

    About Complete

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,7 @@ import Footer from "../../Component/Footer/Footer";
 import { Link } from "react-router-dom";
 const About = () => {
   return (
-    <div className="">
+    <div className="mx-6">
       <div className="text-sm breadcrumbs">
         <ul>
           <li>
@@ -15,10 +15,10 @@ const About = () => {
       <div>
       <div className="card" style={{border:"1px solid #E5E7EB"}}>
   <div className="card-body">
-    <h2 className="card-title text-5xl font-bold mb-12">About us</h2>
-    <h2 className="text-xl font-medium mb-6">About us:</h2>
+    <h2 className="card-title text-3xl md:text-5xl font-bold mb-8 md:mb-10">About us</h2>
+    <h2 className="text-xl font-medium mb-4">About us:</h2>
     <p className="text-base font-normal mb-7">ToolsFinder is a collective of entrepreneurs with global perspectives who believe in AI for good. We believe in a future where AI enhances every professional's toolkit. Our team delves deep into tools, plugins and apps to deliver high quality content with transformative potential right to your screens. Tailored for the proactive modern professional, we provide comprehensive resources that illuminate the path to AI integration in the workplace. Create a free account and elevate your professional journey with ToolsFinder.</p>
-    <h2 className="text-xl font-medium mb-6">Our mission:</h2>
+    <h2 className="text-xl font-medium mb-4">Our mission:</h2>
     <p className="text-base font-normal mb-7">We strive to become the best resource for proactive professionals who want to discover, understand and adopt AI for work.</p>
     <div className="card-actions justify-end">
     </div>
