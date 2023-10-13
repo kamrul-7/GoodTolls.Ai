f18cd96f4af77b02b5fbaeed6338061f82ca1e6e
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-14 01:27:46 +0600

    Scrollbar hide complete

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Component/Navbar/Navbar";
 import AuthProvider from '../Pages/Context/AuthProvider';
+import './Main.css'
 const Main = () => {
 
 
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="max-w-screen-xl mx-auto h-screen overflow-y-scroll hide-scrollbar">
       <AuthProvider>
       <Navbar />
       <Outlet></Outlet>
