531e574ec4de16444eb17a7f75848758790f5122
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-29 04:00:25 +0600

    bug fixed

--- src/Pages/Rate/Rate.jsx
+++ src/Pages/Rate/Rate.jsx
@@ -241,7 +241,7 @@ const Rate = ({id,name}) => {
 
                     <div className="relative">
                         {/* Rate item */}
-                        <div className="md:absolute top-0 right-0 p-11 mt-7 md:mt-0 border rounded-[20px] w-full md:w-[535px] h-fit md:h-[375px]">
+                        <div className="md:absolute top-0 right-0 p-11 mt-7 md:mt-0 border rounded-[20px] w-full md:w-[535px] h-fit md:h-fit">
                             <div className="text-2xl font-bold mb-[20px]">What do you think about {name}?</div>
                             <div className="text-sm mb-[32px]">If you have a moment, it would be greatly appreciated if you<br />could leave a review to share your thoughts with the<br />community. Your feedback is valuable to us and helps us<br />improve our services. Thank you!</div>
                             {/* Stars */}
